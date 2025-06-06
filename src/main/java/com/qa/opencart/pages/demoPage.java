ce9c05ce3cb3e670266e1ac8076b03712d97f66b
Damosjohn <98616831+Damosjohn@users.noreply.github.com>
2025-06-05 19:00:42 -0500

    update My Codes

---
+++ src/main/java/com/qa/opencart/pages/demoPage.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.pages;
+
+public class demoPage {
+
+	public static void main(String[] args) {
+		int c=22;
+		int p=21;
+		int sum=c+p;
+		System.out.println(sum);
+
+	}
+
+}
